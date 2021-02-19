14a047223911613a552430ba75d32839a4aa476e
benjatestaferri7 <benjamintestaferrig@gmail.com>
2021-02-18 21:40:38 -0300

    allowing uploading images to chats

--- src/Components/MyMessage.jsx
+++ src/Components/MyMessage.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const MyMessage = ({ message }) => {
   //if the message is greater than 0, it is an image
-  if (message?.attatchments?.length > 0) {
+  if (message?.attachments?.length > 0) {
     return (
       <img
-        src={message.attatchments[0].file}
-        alt="message-attatchment"
+        src={message.attachments[0].file}
+        alt="message-attachment"
         className="message-image"
         style={{ float: 'right' }}
       />
     );
   }
-  
+
   return (
     <div
       className="message"
